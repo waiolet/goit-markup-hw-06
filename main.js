e74c4d2c423ee3518d3b8f4a2a608c56b90d32f0
Waiolet <violet.bondarenko@gmail.com>
2024-07-10 20:57:33 +0200

    add js

--- main.js
+++ main.js
@@ -11,3 +11,18 @@ document.addEventListener('DOMContentLoaded', function() {
         mobMenuContainer.classList.remove('is-open');
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const orderService = document.querySelector('.order-service');
+    const contactUsOpen = document.querySelector('.backdrop');
+    const contactUsClose = document.querySelector('.backdrop-btn-close');
+
+    orderService.addEventListener('click', function() {
+        contactUsOpen.classList.add('is-open');
+    });
+
+    contactUsClose.addEventListener('click', function() {
+        contactUsOpen.classList.remove('is-open');
+    });
+});
